8e6544d1326a462af90fb30db394dae15a4aba3a
Aleksandar Ogrizovic <aleksandar.ogrizovic@symphony.is>
2017-10-23 13:20:06 +0200

    Dummy Backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,81 @@
 import 'rxjs/add/operator/finally';
 
 import {Component, OnInit} from '@angular/core';
-
-import {QuoteService} from './quote.service';
+import {GoActionsService, Turn} from '../core/go_actions/go_actions.service';
 
 const WGo = require('wgo');
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [GoActionsService]
 })
 
 export class HomeComponent implements OnInit {
 
-  quote: string;
   isLoading: boolean;
   turn: string;
+  player: boolean;
+  board: any;
+
   getTurn = () => {
-    return this.turn === "W"
+    return this.player
       ?
-      "WHITE's Turn"
+      "Your Turn"
       :
-      "BLACK's Turn"
+      "Opponents Turn"
   };
 
-  constructor(private quoteService: QuoteService) {
+  constructor(private goActionsService: GoActionsService) {
   }
 
   ngOnInit() {
     this.turn = "W";
-    this.isLoading = true;
-    this.quoteService.getRandomQuote({category: 'dev'})
-      .finally(() => {
-        this.isLoading = false;
-      })
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
     this.drawBoard();
+    this.decideMyColor();
   }
 
+  private swapPlayers = () => {
+    this.player = !this.player;
+    this.turn = this.turn === "W" ? "B" : "W";
+  };
+
+  decideMyColor = () => {
+    this.goActionsService.assignColor()
+      .subscribe((turn: boolean) => this.player = turn);
+    if (!this.player) {
+      this.receiveTurn();
+    }
+  };
+
+  submitTurn = (turn: Turn) => {
+    this.isLoading = true;
+    this.goActionsService.submitTurn(turn)
+      .finally(() => {
+        this.isLoading = false
+      })
+      .subscribe((myTurn: Turn) => {
+        console.log(myTurn);
+      });
+  };
+
+  receiveTurn = () => {
+    this.isLoading = true;
+    this.goActionsService.receiveTurn(this.board)
+      .finally(() => {
+        this.isLoading = false;
+        this.swapPlayers();
+      })
+      .subscribe((turn: Turn) => {
+        this.board.addObject(this.getCurrentFigure(turn.x, turn.y));
+      });
+  };
+
+  /**
+   * Method for rendering helper coordinates (1-19, A-T)
+   * @returns {{grid: {draw: ((args: any, board: any) => any)}}}
+   */
   private drawCoordinates = () => {
     return {
       grid: {
@@ -76,11 +111,30 @@ export class HomeComponent implements OnInit {
     }
   };
 
+
+  /**
+   * This method depending on whose turn it is returns black or white figure
+   * @param {number} x
+   * @param {number} y
+   * @returns {{x: number; y: number; c: any}}
+   */
+
+  private getCurrentFigure = (x: number, y: number) => {
+    return {
+      x: x,
+      y: y,
+      c: WGo[this.turn]
+    }
+  };
+
+  /**
+   * Draws board at beginning of application
+   */
   private drawBoard = () => {
     const offset = -0.5;
     const width = 600;
 
-    let board = new WGo.Board(document.getElementById("board"), {
+    this.board = new WGo.Board(document.getElementById("board"), {
       width: width,
       section: {
         top: offset,
@@ -90,16 +144,20 @@ export class HomeComponent implements OnInit {
       }
     });
 
-    board.addCustomObject(this.drawCoordinates());
-
-    board.addEventListener("click", (x: number, y: number) => {
-      if (!board.obj_arr[x][y].length) {
-        board.addObject({
+    this.board.addCustomObject(this.drawCoordinates());
+    this.board.addEventListener("click", (x: number, y: number) => {
+      if (!this.board.obj_arr[x][y].length && this.player) {
+        this.board.addObject(this.getCurrentFigure(x, y));
+        const turn: Turn = {
           x: x,
           y: y,
-          c: WGo[this.turn]
-        });
-        this.turn = this.turn === "W" ? "B" : "W";
+          player: this.player
+        };
+        if (this.player) {
+          this.submitTurn(turn);
+        }
+        this.swapPlayers();
+        this.receiveTurn();
       }
     });
   };
