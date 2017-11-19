e32a2d411d5a57dfa5df57a9f556db226533513c
Aleksandar Ogrizovic <aleksandar.ogrizovic@symphony.is>
2017-11-19 13:30:07 +0100

    Chrome headless not working. Reverting to phantom js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   config.set({
@@ -12,8 +11,7 @@ module.exports = function(config) {
       require('karma-phantomjs-launcher'),
       require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma'),
-      require('karma-chrome-launcher'),
+      require('@angular/cli/plugins/karma')
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -39,7 +37,7 @@ module.exports = function(config) {
     // Level of logging, can be: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
